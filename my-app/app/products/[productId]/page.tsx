3cee95f5e2883d1fb83bdfe977ced9c0a767f070
mahek-kariya-noovosoft <mahek.kariya@noovosoft.com>
2026-01-29 14:23:40 +0530

    add generateStaticParams use and incremental static regenration (using npm run build and npm run start)

--- my-app/app/products/[productId]/page.tsx
+++ my-app/app/products/[productId]/page.tsx
@@ -1,25 +1,56 @@
 import ProductClient from "./ProductClient";
+import {notFound} from "next/navigation";
 
 type PageProps = {
     params: Promise<{
         productId: string;
-    }>
+    }>;
 };
+
+// 1️⃣ Pre-build ONLY top 3 products
+export async function generateStaticParams() {
+    return [
+        {productId: "1"},
+        {productId: "2"},
+        {productId: "3"},
+    ];
+}
+
+// 2️⃣ Allow other product pages at runtime
+export const dynamicParams = true;
+
+// 3️⃣ Revalidate every 10 seconds
+export const revalidate = 10;
 
 export default async function ProductPage({params}: PageProps) {
     const {productId} = await params;
 
+    // fake DB fetch
     const product = await getProduct(productId);
 
+    if (!product) {
+        notFound();
+    }
+
     return (
-        <ProductClient
-            productId={product.id}
-            productName={product.name}
-        />
+        <>
+            {/* 🔥 SERVER-ONLY PROOF */}
+            <p style={{color: "gray"}}>
+                Page generated at: <strong>{new Date().toLocaleTimeString()}</strong>
+            </p>
+
+            <ProductClient
+                productId={product.id}
+                productName={product.name}
+            />
+        </>
     );
 }
 
 async function getProduct(id: string) {
+    // simulate invalid product
+    if (Number(id) > 20) return null;
+
     return {
         id,
         name: `Product ${id}`,

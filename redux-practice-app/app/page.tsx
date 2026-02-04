49975e56e19ad3ea4b4c59de3ba9db85c35c38d9
mahek-kariya-noovosoft <mahek.kariya@noovosoft.com>
2026-02-04 15:48:20 +0530

    create userSlice

--- redux-practice-app/app/page.tsx
+++ redux-practice-app/app/page.tsx
@@ -1,10 +1,24 @@
-import type { Metadata } from "next";
-import { Counter } from "./components/counter/Counter";
+"use client"
 
-export default function IndexPage() {
-  return <Counter />;
+import {useDispatch, useSelector} from "react-redux"
+import type {RootState, AppDispatch} from "../store"
+import {login, logout} from "@/store/userSlice";
+
+export default function Page() {
+    const user = useSelector((state: RootState) => state.user)
+    const dispatch = useDispatch<AppDispatch>()
+
+    return (
+        <>
+            <p>{user.isLoggedIn ? `Hi ${user.name}` : "Not logged in"}</p>
+
+            <button onClick={() => dispatch(login("Mahek"))}>
+                Login
+            </button>
+
+            <button onClick={() => dispatch(logout())}>
+                Logout
+            </button>
+        </>
+    )
 }
-
-export const metadata: Metadata = {
-  title: "Redux Toolkit",
-};
